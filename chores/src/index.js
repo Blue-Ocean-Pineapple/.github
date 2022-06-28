33e5f94600b7c477df7d117021415ff181610454
Brian Bui <brianbui012@gmail.com>
2022-06-28 14:23:01 -0700

    Removed chakara from main app index.js file

--- chores/src/index.js
+++ chores/src/index.js
@@ -6,8 +6,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
