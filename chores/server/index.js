ee6aef11953cc7dfec4c198464f64333553077a4
Barry Cheung <barryleecheung@gmail.com>
2022-06-28 14:08:53 -0700

    Install cors

--- chores/server/index.js
+++ chores/server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+var cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,7 +10,7 @@ var router = require('./routes/index.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use('/', router);
 app.use(express.static('client/dist'));
 
