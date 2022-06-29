d811a63a48514d51e21cedac57356b5a06bcf3a2
Brian Bui <brianbui012@gmail.com>
2022-06-28 19:22:53 -0700

    Commiting for deployment

--- chores/src/components/student/student.jsx
+++ chores/src/components/student/student.jsx
@@ -28,8 +28,14 @@ export default function Student(props) {
 
   return (
     <ChakraProvider theme={theme}>
-      <Box boxShadow="base" maxWidth="70%" m="auto">
-        <TableContainer>
+      <Box
+        overflowY="auto"
+        maxH="500px"
+        boxShadow="base"
+        maxWidth="70%"
+        m="auto"
+      >
+        <TableContainer maxH="60%" overflowY="scroll">
           <Table variant="simple" size="sm">
             <TableCaption placement="top">Student's Tickets</TableCaption>
             <Thead>
@@ -37,45 +43,308 @@ export default function Student(props) {
                 <Th>Task Name</Th>
                 <Th>Owner of Ticket</Th>
                 <Th>Location</Th>
+                <Th>Time</Th>
                 <Th>ID</Th>
-                <Th>Vote</Th>
-                <Th>Mark Complete</Th>
+                <Th textAlign="center">Vote</Th>
+                <Th textAlign="center">Mark Complete</Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
                 <Td>Mowing the Lawn</Td>
-                <Td>Brian</Td>
+                <Td>Brian Bui</Td>
                 <Td>San Jose</Td>
-                <Td>2</Td>
-                <Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
                   <Button colorScheme="blue">
                     <BsHandThumbsUp />
-                  </Button>{" "}
+                  </Button>
                   <Button colorScheme="red">
                     <BsHandThumbsDown />
                   </Button>
                 </Td>
-                <Td>
+                <Td textAlign="center">
                   <Button colorScheme="green">
                     <BsCheckLg />
                   </Button>
                 </Td>
               </Tr>
               <Tr>
-                <Td>Task Number 2</Td>
-                <Td>Brian Bui</Td>
-                <Td>San Jose What if this was longer</Td>
-                <Td>2</Td>
-                <Td>
+                <Td>Finding My Cat</Td>
+                <Td>Christy Lopez</Td>
+                <Td>San Jose</Td>
+                <Td>6/29 at 4:15 PM</Td>
+                <Td>2313</Td>
+                <Td display="flex" justifyContent="space-around">
                   <Button colorScheme="blue">
                     <BsHandThumbsUp />
-                  </Button>{" "}
+                  </Button>
                   <Button colorScheme="red">
                     <BsHandThumbsDown />
                   </Button>
                 </Td>
-                <Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
+                  <Button colorScheme="green">
+                    <BsCheckLg />
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Mowing the Lawn</Td>
+                <Td>Brian Bui</Td>
+                <Td>San Jose</Td>
+                <Td>6/28 at 6:00 PM</Td>
+                <Td>8347</Td>
+                <Td display="flex" justifyContent="space-around">
+                  <Button colorScheme="blue">
+                    <BsHandThumbsUp />
+                  </Button>
+                  <Button colorScheme="red">
+                    <BsHandThumbsDown />
+                  </Button>
+                </Td>
+                <Td textAlign="center">
                   <Button colorScheme="green">
                     <BsCheckLg />
                   </Button>
