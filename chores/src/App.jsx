2d5582f44cee4170e87f45be884f686692a874c6
pegaatron <ppt003@ucsd.edu>
2022-06-28 19:26:49 -0700

    fixed conflicts

--- chores/src/App.jsx
+++ chores/src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 // import Login from './components/login/login.jsx';
 // import Register from './components/register/Register.jsx';
 // import Home from './components/home/home.jsx';
 // import Profile from './components/profile/Profile.jsx';
-=======
-import Login from './components/login/Login.jsx';
-import Register from './components/register/Register.jsx';
-import Home from './components/home/Home.jsx';
-import Profile from './components/profile/Profile.jsx';
-import Admin from './components/admin/admin.jsx'
->>>>>>> main
 // import Admin from './components/admin/admin.jsx';
 // import Customer from './components/customer/customer.jsx';
 import Map from './components/map/map.jsx';
@@ -30,7 +22,6 @@ function App(props) {
   const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
 
   return (
-<<<<<<< HEAD
     // <div>
     //   <Map/>
     // </div>
@@ -49,25 +40,6 @@ function App(props) {
     //   </Routes>
     //  </Router>
     // </AuthContextProvider>
-=======
-    <AuthContextProvider>
-     <Router>
-      <Routes>
-         <Route exact path="/" element={<Home setIsAuth={setIsAuth}/>} />
-         <Route path="/login" element={!isAuth ? <Login setIsAuth={setIsAuth} /> : <Home />}/>
-         <Route path="/register" element={!isAuth ? <Register setIsAuth={setIsAuth} /> : <Home />}/>
-         <Route path="/profile" element={!isAuth ? <Profile /> : <Home />}/>
-          <Route path="/admin" element={!isAuth ? <Admin setIsAuth={setIsAuth} /> : <Admin />}/>
-         {/*<Route path="/customer" element={!isAuth ? <Login setIsAuth={setIsAuth} /> : <Customer />}/>
-         <Route path="/staff" element={!isAuth ? <Login setIsAuth={setIsAuth} /> : <Staff />}/>
-         <Route path="/student" element={!isAuth ? <Login setIsAuth={setIsAuth} /> : <Student />}/>
-         <Route path="/map" element={!isAuth ? <Login setIsAuth={setIsAuth}/> : <Map />} /> */}
-           {/* <Customer /> */}
-      </Routes>
-     </Router>
-    </AuthContextProvider>
-
->>>>>>> main
   );
 }
 
